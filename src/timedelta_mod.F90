63320bb629b09be66b21871a274c7a30e68625b3
Li Dong <dongli@lasg.iap.ac.cn>
2021-02-16 22:17:48 +0800

    Add condition operators for datetime_type

--- src/timedelta_mod.F90
+++ src/timedelta_mod.F90
@@ -20,6 +20,18 @@ module timedelta_mod
     procedure :: total_hours
     procedure :: total_days
     procedure :: negate
+    procedure :: eq
+    procedure :: neq
+    procedure :: gt
+    procedure :: ge
+    procedure :: lt
+    procedure :: le
+    generic :: operator(==) => eq
+    generic :: operator(/=) => neq
+    generic :: operator(>) => gt
+    generic :: operator(>=) => ge
+    generic :: operator(<) => lt
+    generic :: operator(<=) => le
   end type timedelta_type
 
 contains
@@ -105,53 +117,49 @@ contains
 
   end function create_timedelta
 
-  real(8) function total_seconds(this)
+  pure real(8) function total_seconds(this)
 
     class(timedelta_type), intent(in) :: this
 
     if (this%months == 0.0d0) then
       total_seconds = this%days * 86400 + this%hours * 3600 + this%minutes * 60 + this%seconds + this%milliseconds * 1.0d-3
     else
-      write(*, *) '[Error]: timedelta has nonzero months value!'
       total_seconds = -1
     end if
 
   end function total_seconds
 
-  real(8) function total_minutes(this)
+  pure real(8) function total_minutes(this)
 
     class(timedelta_type), intent(in) :: this
 
     if (this%months == 0.0d0) then
       total_minutes = this%days * 1440 + this%hours * 60 + this%minutes + (this%seconds + this%milliseconds * 1.0d-3) / 60.0d0
     else
-      write(*, *) '[Error]: timedelta has nonzero months value!'
       total_minutes = -1
     end if
 
   end function total_minutes
 
-  real(8) function total_hours(this)
+  pure real(8) function total_hours(this)
 
     class(timedelta_type), intent(in) :: this
 
     if (this%months == 0.0d0) then
       total_hours = this%days * 24 + this%hours + (this%minutes + (this%seconds + this%milliseconds * 1.0d-3) / 60.0d0) / 60.0d0
     else
-      write(*, *) '[Error]: timedelta has nonzero months value!'
       total_hours = -1
     end if
 
   end function total_hours
 
-  real(8) function total_days(this)
+  pure real(8) function total_days(this)
 
     class(timedelta_type), intent(in) :: this
 
     if (this%months == 0.0d0) then
       total_days = this%days + (this%hours + (this%minutes + (this%seconds + this%milliseconds * 1.0d-3) / 60.0d0) / 60.0d0) / 24.0d0
     else
-      write(*, *) '[Error]: timedelta has nonzero months value!'
       total_days = -1
     end if
 
@@ -168,5 +176,59 @@ contains
     res%milliseconds = - this%milliseconds
 
   end function negate
+
+  pure elemental logical function eq(this, other)
+
+    class(timedelta_type), intent(in) :: this
+    class(timedelta_type), intent(in) :: other
+
+    eq = this%total_seconds() == other%total_seconds()
+
+  end function eq
+
+  pure elemental logical function neq(this, other)
+
+    class(timedelta_type), intent(in) :: this
+    class(timedelta_type), intent(in) :: other
+
+    neq = this%total_seconds() /= other%total_seconds()
+
+  end function neq
+
+  pure elemental logical function gt(this, other)
+
+    class(timedelta_type), intent(in) :: this
+    class(timedelta_type), intent(in) :: other
+
+    gt = this%total_seconds() > other%total_seconds()
+
+  end function gt
+
+  pure elemental logical function ge(this, other)
+
+    class(timedelta_type), intent(in) :: this
+    class(timedelta_type), intent(in) :: other
+
+    ge = this%total_seconds() >= other%total_seconds()
+
+  end function ge
+
+  pure elemental logical function lt(this, other)
+
+    class(timedelta_type), intent(in) :: this
+    class(timedelta_type), intent(in) :: other
+
+    lt = this%total_seconds() < other%total_seconds()
+
+  end function lt
+
+  pure elemental logical function le(this, other)
+
+    class(timedelta_type), intent(in) :: this
+    class(timedelta_type), intent(in) :: other
+
+    le = this%total_seconds() <= other%total_seconds()
+
+  end function le
 
 end module timedelta_mod
